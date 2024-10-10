7a9ab464478fba9e825e5b0216260bb7cec6a628
Yunhao Deng <yunhao.deng@kuleuven.be>
2024-10-10 20:15:00 +0200

    Width mismatch fix

--- occamy_spi_slave.sv
+++ occamy_spi_slave.sv
@@ -91,14 +91,14 @@ module occamy_spi_slave #(
       .axi_master_r_data(axi_lite_rsp_i.r.data),
       .axi_master_r_resp(axi_lite_rsp_i.r.resp),
       .axi_master_r_last('1),
-      .axi_master_r_id(1),
+      .axi_master_r_id(1'd1),
       .axi_master_r_user('0),
       .axi_master_r_ready(axi_lite_req_o.r_ready),
 
       // WRITE RESPONSE CHANNEL
       .axi_master_b_valid(axi_lite_rsp_i.b_valid),
       .axi_master_b_resp(axi_lite_rsp_i.b.resp),
-      .axi_master_b_id(1),
+      .axi_master_b_id(1'd1),
       .axi_master_b_user('0),
       .axi_master_b_ready(axi_lite_req_o.b_ready)
   );
